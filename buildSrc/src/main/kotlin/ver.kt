37964e0d76523eef655f72c1a391bbe8c66bb5e0
kw.ryu <kw.ryu@anypointmedia.com>
2021-12-16 10:37:15 +0900

    Update to the latest kotlin, gradle

--- buildSrc/src/main/kotlin/ver.kt
+++ buildSrc/src/main/kotlin/ver.kt
@@ -4,7 +4,7 @@ object ver {
     const val artifactId = "pkt"
 
     // classpath
-    const val kotlin = "1.6.0"
+    const val kotlin = "1.6.10"
     const val android_tools_build_gradle = "7.0.4"
     const val gradle_versions_plugin = "0.39.0"
 
